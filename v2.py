f6f87e582f9416da1b9ab199c8bc8ea18122dabb
Robosword <robotanmol@gmail.com>
2026-09-14 10:14:05 -0700

    Add motor initialization and update Robot class
    Initialize motors in Drivebase and update Robot constructor.

--- v2.py
+++ v2.py
@@ -16,8 +16,6 @@ def initializeRandomSeed():
     
 initializeRandomSeed()
 
-from vex import *
-
 #dictionary for x's and y's
 colliders = {}
 
@@ -32,15 +30,29 @@ class Remote:
 
 class Drivebase:
     def __init__(self):
-        pass
-
+        self.motor1 = Motor(Ports.PORT1, False)
+        self.motor2 = Motor(Ports.PORT2, False)
+    
+    def motor1Encodding(self):
+        return self.motor1.position(DEGREES)
+    
+    def motor2Encodding(self):
+        return self.motor2.position(DEGREES)
+    
+    def forward(self):
+        self.motor1.spin(FORWARD)
+        self.motor2.spin(FORWARD)
+    
 class IntakeFlywheel:
     def __init__(self):
         pass
 
 class Robot:
-    def __init__(self):
-        pass
+    def __init__(self, brain, inertial):
+        self.brain = brain
+        self.inertial = inertial
+        self.x = 0
+        self.y = 0
     
     def odometry(self):
         pass
